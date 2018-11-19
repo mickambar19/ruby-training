f77ad8198333a9f07747c8864e6a7e1ec2e6bbe0
Alexis Jimenez <mick_ambar19@hotmail.com>
2018-11-19 15:13:57 -0600

    titled files and finishing them

--- ex2.rb
+++ ex2.rb
@@ -1,23 +1,3 @@
-puts "I will now count my chickens:"
-
-puts "Hens #{ 25 + 30 / 6}"
-puts "Roosters #{100.0 - 25 * 3 % 4}"
-
-puts "Now will count the eggs:"
-
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6.0  
-
-puts "Is it true that 3 + 2 > 5 - 7?"
-
-puts 3 + 2 < 5 - 7
-
-puts "What is 3 + 2? #{3 + 2}"
-puts "What is 3 + 2? #{5 - 7}" 
-
-puts "Oh, that's why it's false."
-
-puts "How about some more."
-
-puts "Is it greater? #{5 > -2 }"
-puts "Is it greater or equal? #{5 >= -2}"
-puts "Is it less or equal? #{5 <= -2}"
+#Comments and pound characters
+# A comment, this is so you can read your program later.
+puts "I could have code like this." # and the comment after is ingnored
---
+++ ex3.rb
@@ -0,0 +1,24 @@
+#Numbers and math
+puts "I will now count my chickens:"
+
+puts "Hens #{ 25 + 30 / 6}"
+puts "Roosters #{100.0 - 25 * 3 % 4}"
+
+puts "Now will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6.0  
+
+puts "Is it true that 3 + 2 > 5 - 7?"
+
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 3 + 2? #{5 - 7}" 
+
+puts "Oh, that's why it's false."
+
+puts "How about some more."
+
+puts "Is it greater? #{5 > -2 }"
+puts "Is it greater or equal? #{5 >= -2}"
+puts "Is it less or equal? #{5 <= -2}"

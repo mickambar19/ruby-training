a66409e84b93917b7d6b16fac206a24691847180
Alexis Jimenez <mick_ambar19@hotmail.com>
2018-11-19 16:27:19 -0600

    Added examples with variables and printing

---
+++ ex5.rb
@@ -0,0 +1,19 @@
+# More Variables and Printing
+
+my_name = 'Alexander'
+my_age = 23
+my_height = 174 
+my_weight = 74
+my_eyes = 'Brown'
+my_teeth = "White"
+my_hair = "Black"
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair"
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."

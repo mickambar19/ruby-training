6993066e0b28138a06dfaf94709d7c02f45eec96
Amjimenez <alexis.m.jimenez.o@gmail.com>
2018-12-23 12:40:35 -0600

    Exercise 13: Parameters, Unpacking, Variables

--- ex12.rb
+++ ex12.rb
@@ -10,3 +10,17 @@ number = another.to_i
 
 smaller = number / 100
 puts "A smaller number is #{smaller}."
+
+puts "NOW WITH to_f"
+print "Give me a number: "
+number = gets.chomp.to_f
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_f
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."

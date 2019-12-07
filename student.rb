9178920aa55ce022948e1676684deb9784a3504c
Alexis Jimenez <ajimenez@wiggot.com>
2019-12-06 20:19:21 -0600

    :sparkles: Added attribute accessor and attribute reader.

--- student.rb
+++ student.rb
@@ -1,17 +1,12 @@
 class Student
+  attr_accessor :first_name, :last_name, :email
+  attr_reader :username
+
   @first_name
   @last_name
   @email
   @username
   @password
-
-  def first_name=(name)
-   @first_name = name 
-  end
-
-  def first_name
-    @first_name
-  end
 
   def to_s
     "First name: #{@first_name}"
@@ -21,5 +16,12 @@ end
 student1 = Student.new
 puts student1
 student1.first_name = "Alexis"
+student1.last_name = "Williamson"
+student1.email = "alexis@yopmail.com"
+student1.username = "mickambar19"
+
 puts student1
+puts "=== Student info ==="
 puts student1.first_name
+puts student1.last_name
+puts student1.email

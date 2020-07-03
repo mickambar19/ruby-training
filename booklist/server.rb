a2765495010481483e6c934d14271e6c8d007209
Alexis Jimenez <ajimenez@wiggot.com>
2020-07-03 15:18:11 -0500

    :sparkles: Added docker compose and api to find books.

---
+++ booklist/server.rb
@@ -0,0 +1,42 @@
+require 'sinatra'
+require 'sinatra/namespace'
+require 'mongoid'
+
+Mongoid.load! "mongoid.config"
+
+class Book
+  include Mongoid::Document
+  field :title, type: String
+  field :author, type: String
+  field :isbn, type: String
+  
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :isbn, presence: true
+
+  index({ title: 'text' })
+  index({ isbn: 1}, {unique: true, name: "isbn_index"})
+
+  scope :title, -> (title) { where(title: /^#{title}/) }
+  scope :isbn, -> (isbn) { where(isbn: isbn) }
+  scope :author, -> (author) { where(author: author) }
+end
+
+get '/' do
+  'Welcome to BookList!'
+end
+
+namespace '/api/v1' do
+  before do
+    content_type 'application/json'
+  end
+
+  get '/books' do
+    books = Book.all
+    
+    [:title, :isbn, :author].each do |filter|
+      books = books.send(filter, params[filter]) if params[filter]
+    end
+    books.to_json
+  end
+end
